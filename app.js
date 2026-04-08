49e75bf1f6f59964b9c52826404b6059b7383782
Martina <martinamoneo2@gmail.com>
2026-04-08 18:52:54 -0300

    US5 lista

--- app.js
+++ app.js
@@ -2,31 +2,39 @@ const express = require('express');
 const path = require('path'); // Esta librería ayuda con las rutas de carpetas
 const app = express();
 
-
 // --- CONFIGURACIÓN ---
+app.set('view engine', 'ejs');// 1. Le decimos a Express que use EJS
 
-// 1. Le decimos a Express que use EJS
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));// 2. Le decimos dónde están tus páginas (basado en tu carpeta views/pages)
 
-// 2. Le decimos dónde están tus páginas (basado en tu carpeta views/pages)
-app.set('views', path.join(__dirname, 'views'));
-
-// 3. Carpeta para archivos estáticos (CSS, Imágenes, etc.)
-// Esto hace que tu carpeta 'assets' sea pública
-app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'public')));// Esto hace que tu carpeta 'public' sea pública
 
 
-// 1. Página de Inicio
+// HOME
 app.get('/', (req, res) => {
-    res.render('pages/index', { esInicio: true }); 
+    const productos = [
+        { nombre: 'Producto', precio: 19900, imagen: 'pinterest.jpg' },
+        { nombre: 'Producto', precio: 19900, imagen: 'pinterest.jpg' },
+        { nombre: 'Producto', precio: 19900, imagen: 'pinterest.jpg' },
+        { nombre: 'Producto', precio: 19900, imagen: 'pinterest.jpg' },
+        { nombre: 'Producto', precio: 19900, imagen: 'pinterest.jpg' },
+        { nombre: 'Producto', precio: 19900, imagen: 'pinterest.jpg' },
+        { nombre: 'Producto', precio: 19900, imagen: 'pinterest.jpg' },
+        { nombre: 'Producto', precio: 19900, imagen: 'pinterest.jpg' }
+    ];
+        
+    res.render('pages/index', { 
+        esInicio: true, 
+        listaProductos: productos // Le pasamos la lista a la vista
+    });
 });
 
-// 2. Registro de Usuario
+// REGISTER
 app.get('/register', (req, res) => {
     res.render('pages/register', { esInicio: false });
 });
 
-// 3. Inicio de Sesión
+// LOGIN
 app.get('/login', (req, res) => {
     res.render('pages/login' , { esInicio: false });
 });
