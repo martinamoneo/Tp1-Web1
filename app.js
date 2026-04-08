eb55641f0c345697f392d8b10b7fca67d2f20df3
Martina <martinamoneo2@gmail.com>
2026-04-08 16:53:12 -0300

    US5

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const path = require('path'); // Esta librería ayuda con las rutas de carpetas
 const app = express();
 
+
 // --- CONFIGURACIÓN ---
 
 // 1. Le decimos a Express que use EJS
 app.set('view engine', 'ejs');
 
 // 2. Le decimos dónde están tus páginas (basado en tu carpeta views/pages)
-app.set('views', path.join(__dirname, 'views/pages'));
+app.set('views', path.join(__dirname, 'views'));
 
 // 3. Carpeta para archivos estáticos (CSS, Imágenes, etc.)
 // Esto hace que tu carpeta 'assets' sea pública
@@ -17,32 +18,17 @@ app.use(express.static(path.join(__dirname, 'assets')));
 
 // 1. Página de Inicio
 app.get('/', (req, res) => {
-    res.render('index'); 
+    res.render('pages/index', { esInicio: true }); 
 });
 
-// 2. Detalle de Producto
-app.get('/products', (req, res) => {
-    res.render('product');
-});
-
-// 3. Carrito de Compras
-app.get('/cart', (req, res) => {
-    res.render('cart');
-});
-
-// 4. Proceso de Pago
-app.get('/checkout', (req, res) => {
-    res.render('checkout');
-});
-
-// 5. Registro de Usuario
+// 2. Registro de Usuario
 app.get('/register', (req, res) => {
-    res.render('register');
+    res.render('pages/register', { esInicio: false });
 });
 
-// 6. Inicio de Sesión
+// 3. Inicio de Sesión
 app.get('/login', (req, res) => {
-    res.render('login');
+    res.render('pages/login' , { esInicio: false });
 });
 
 // Iniciamos el servidor
